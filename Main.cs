08fe9a90684529aeb686c6bb3d5f6bc370845652
Haplo064 <haplo064@gmail.com>
2020-07-06 13:06:54 +0930

    Ring build
    Added customisable ring around player char.

--- Main.cs
+++ Main.cs
@@ -22,76 +22,31 @@ namespace PixelPerfect
         public Num.Vector4 col = new Num.Vector4(1f, 1f, 1f, 1f);
         public Num.Vector4 col2 = new Num.Vector4(0.4f, 0.4f, 0.4f, 1f);
 
+        public bool ring = false;
+        public Num.Vector4 col_ring = new Num.Vector4(0.4f, 0.4f, 0.4f, 0.5f);
+        public float radius = 10f;
+        public int segments = 100;
+        public float thickness = 10f;
+
+
         public void Initialize(DalamudPluginInterface pI)
         {
             this.pluginInterface = pI;
             Configuration = pluginInterface.GetPluginConfig() as Config ?? new Config();
 
+            ring = Configuration.Ring;
+            thickness = Configuration.Thickness;
+            col_ring = Configuration.Col_Ring;
+            segments = Configuration.Segments;
+            radius = Configuration.Radius;
 
-            try
-            { enabled = Configuration.Enabled; }
-            catch (Exception)
-            {
-                PluginLog.LogError("Failed to set Enabled");
-                enabled = false;
-            }
+            enabled = Configuration.Enabled;
+            combat = Configuration.Combat;
+            circle = Configuration.Circle;
+            instance = Configuration.Instance;
+            col = Configuration.Col;
+            col2 = Configuration.Col2;
 
-            try
-            { combat = Configuration.Combat; }
-            catch (Exception)
-            {
-                PluginLog.LogError("Failed to set Combat");
-                combat = true;
-            }
-
-            try
-            { circle = Configuration.Circle; }
-            catch (Exception)
-            {
-                PluginLog.LogError("Failed to set Circle");
-                circle = false;
-            }
-            try
-            { instance = Configuration.Instance; }
-            catch (Exception)
-            {
-                PluginLog.LogError("Failed to set Instance");
-                instance = false;
-            }
-
-            try
-            {
-                if (Configuration.Col != null)
-                {
-                    col = Configuration.Col;
-                }
-                else
-                {
-                    col = new Num.Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-                }
-            }
-            catch (Exception)
-            {
-                PluginLog.LogError("Failed to set col");
-                col = new Num.Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-            }
-
-            try
-            {
-                if (Configuration.Col2 != null)
-                {
-                    col2 = Configuration.Col2;
-                }
-                else
-                {
-                    col2 = new Num.Vector4(0.4f, 0.4f, 0.4f, 1.0f);
-                }
-            }
-            catch (Exception)
-            {
-                PluginLog.LogError("Failed to set col2");
-                col = new Num.Vector4(0.4f, 0.4f, 0.4f, 1.0f);
-            }
 
             this.pluginInterface.UiBuilder.OnBuildUi += DrawWindow;
             this.pluginInterface.UiBuilder.OnOpenConfigUi += ConfigWindow;
@@ -112,12 +67,21 @@ namespace PixelPerfect
             {
                 ImGui.SetNextWindowSize(new Num.Vector2(300, 500), ImGuiCond.FirstUseEver);
                 ImGui.Begin("Pixel Perfect Config", ref config);
-                ImGui.Checkbox("Enable", ref enabled);
+                ImGui.Checkbox("Hitbox", ref enabled);
                 ImGui.Checkbox("Outer Ring", ref circle);
                 ImGui.Checkbox("Combat Only", ref combat);
                 ImGui.Checkbox("Instance Only", ref instance);
                 ImGui.ColorEdit4("Colour", ref col, ImGuiColorEditFlags.NoInputs);
                 ImGui.ColorEdit4("Outer Colour", ref col2, ImGuiColorEditFlags.NoInputs);
+
+                ImGui.Separator();
+                
+                ImGui.Checkbox("Ring", ref ring);
+                ImGui.DragFloat("Yalms", ref radius);
+                ImGui.DragFloat("Thickness", ref thickness);
+                ImGui.DragInt("Smoothness", ref segments);
+                ImGui.ColorEdit4("Ring Colour", ref col_ring, ImGuiColorEditFlags.NoInputs);
+
                 if (ImGui.Button("Save and Close Config"))
                 {
                     SaveConfig();
@@ -159,6 +123,15 @@ namespace PixelPerfect
                     }
 
                     ImGui.End();
+
+                    if (ring)
+                    {
+                        ImGui.Begin("Ring", ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoNav | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoBackground);
+                        ImGui.SetWindowPos(new Num.Vector2(0, 0));
+                        ImGui.SetWindowSize(new Num.Vector2(1920, 1080));
+                        DrawRingWorld(pluginInterface.ClientState.LocalPlayer, radius, segments, thickness, ImGui.GetColorU32(col_ring));
+                        ImGui.End();
+                    }
                 }
             }
         }
@@ -179,8 +152,26 @@ namespace PixelPerfect
             Configuration.Instance = instance;
             Configuration.Col = col;
             Configuration.Col2 = col2;
+            Configuration.Col_Ring = col_ring;
+            Configuration.Thickness = thickness;
+            Configuration.Segments = segments;
+            Configuration.Ring = ring;
+            Configuration.Radius = radius;
             this.pluginInterface.SavePluginConfig(Configuration);
         }
+
+        public void DrawRingWorld(Dalamud.Game.ClientState.Actors.Types.Actor actor, float radius, int num_segments, float thicc, uint colour)
+        {
+            int seg = num_segments / 2;
+
+            for (int i = 0; i <= num_segments; i++)
+            {
+                pluginInterface.Framework.Gui.WorldToScreen(new SharpDX.Vector3(actor.Position.X + (radius * (float)Math.Sin((Math.PI / seg) * i)), actor.Position.Z, actor.Position.Y + (radius * (float)Math.Cos((Math.PI / seg) * i))), out SharpDX.Vector2 pos);
+                ImGui.GetWindowDrawList().PathLineTo(new Num.Vector2(pos.X, pos.Y));
+            }
+            ImGui.GetWindowDrawList().PathStroke(colour, true, thicc);
+        }
+
     }
 
     public class Config : IPluginConfiguration
@@ -192,6 +183,12 @@ namespace PixelPerfect
         public bool Instance { get; set; } = false;
         public Num.Vector4 Col { get; set; } = new Num.Vector4(1f, 1f, 1f, 1f);
         public Num.Vector4 Col2 { get; set; } = new Num.Vector4(0.4f, 0.4f, 0.4f, 1f);
+        public Num.Vector4 Col_Ring { get; set; } = new Num.Vector4(0.4f, 0.4f, 0.4f, 0.5f);
+        public int Segments { get; set; } = 100;
+        public float Thickness { get; set; } = 10f;
+        public bool Ring { get; set; } = false;
+        public float Radius { get; set; } = 2f;
+
 
     }
 
