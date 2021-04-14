0b426b2d2b44d740f15b518232899cb845ec654d
Haplo064 <haplo064@gmail.com>
2021-04-14 13:50:34 +0930

    Updated for 5.5
    - Code revision included

--- Main.cs
+++ Main.cs
@@ -1,56 +1,50 @@
 ﻿using System;
 using Dalamud.Plugin;
 using ImGuiNET;
+using ImGuiScene;
 using Dalamud.Configuration;
 using Num = System.Numerics;
 using Dalamud.Game.Command;
+using Dalamud.Interface;
 
 namespace PixelPerfect
 {
     public class PixelPerfect : IDalamudPlugin
     {
         public string Name => "Pixel Perfect";
-        private DalamudPluginInterface pluginInterface;
-        public Config Configuration;
-
-        public bool enabled = true;
-        public bool config = false;
-        public bool debug = false;
-        public bool combat = true;
-        public bool circle = false;
-        public bool instance = false;
-        public Num.Vector4 col = new Num.Vector4(1f, 1f, 1f, 1f);
-        public Num.Vector4 col2 = new Num.Vector4(0.4f, 0.4f, 0.4f, 1f);
-
-        public bool ring = false;
-        public Num.Vector4 col_ring = new Num.Vector4(0.4f, 0.4f, 0.4f, 0.5f);
-        public float radius = 10f;
-        public int segments = 100;
-        public float thickness = 10f;
-
-
+        private DalamudPluginInterface _pluginInterface;
+        private Config _configuration;
+        private bool _enabled = true;
+        private bool _config;
+        private bool _combat = true;
+        private bool _circle;
+        private bool _instance;
+        private Num.Vector4 _col = new Num.Vector4(1f, 1f, 1f, 1f);
+        private Num.Vector4 _col2 = new Num.Vector4(0.4f, 0.4f, 0.4f, 1f);
+        private bool _ring;
+        private Num.Vector4 _colRing = new Num.Vector4(0.4f, 0.4f, 0.4f, 0.5f);
+        private float _radius = 10f;
+        private int _segments = 100;
+        private float _thickness = 10f;
+        
         public void Initialize(DalamudPluginInterface pI)
         {
-            this.pluginInterface = pI;
-            Configuration = pluginInterface.GetPluginConfig() as Config ?? new Config();
-
-            ring = Configuration.Ring;
-            thickness = Configuration.Thickness;
-            col_ring = Configuration.Col_Ring;
-            segments = Configuration.Segments;
-            radius = Configuration.Radius;
-
-            enabled = Configuration.Enabled;
-            combat = Configuration.Combat;
-            circle = Configuration.Circle;
-            instance = Configuration.Instance;
-            col = Configuration.Col;
-            col2 = Configuration.Col2;
-
-
-            this.pluginInterface.UiBuilder.OnBuildUi += DrawWindow;
-            this.pluginInterface.UiBuilder.OnOpenConfigUi += ConfigWindow;
-            this.pluginInterface.CommandManager.AddHandler("/pp", new CommandInfo(Command)
+            _pluginInterface = pI;
+            _configuration = _pluginInterface.GetPluginConfig() as Config ?? new Config();
+            _ring = _configuration.Ring;
+            _thickness = _configuration.Thickness;
+            _colRing = _configuration.ColRing;
+            _segments = _configuration.Segments;
+            _radius = _configuration.Radius;
+            _enabled = _configuration.Enabled;
+            _combat = _configuration.Combat;
+            _circle = _configuration.Circle;
+            _instance = _configuration.Instance;
+            _col = _configuration.Col;
+            _col2 = _configuration.Col2;
+            _pluginInterface.UiBuilder.OnBuildUi += DrawWindow;
+            _pluginInterface.UiBuilder.OnOpenConfigUi += ConfigWindow;
+            _pluginInterface.CommandManager.AddHandler("/pp", new CommandInfo(Command)
             {
                 HelpMessage = "Pixel Perfect config."
             });
@@ -58,127 +52,125 @@ namespace PixelPerfect
 
         public void Dispose()
         {
-            this.pluginInterface.UiBuilder.OnBuildUi -= DrawWindow;
-            this.pluginInterface.UiBuilder.OnOpenConfigUi -= ConfigWindow;
-            this.pluginInterface.CommandManager.RemoveHandler("/pp");
+            _pluginInterface.UiBuilder.OnBuildUi -= DrawWindow;
+            _pluginInterface.UiBuilder.OnOpenConfigUi -= ConfigWindow;
+            _pluginInterface.CommandManager.RemoveHandler("/pp");
         }
 
         private void DrawWindow()
         {
-
-            if (config)
+            if (_config)
             {
                 ImGui.SetNextWindowSize(new Num.Vector2(300, 500), ImGuiCond.FirstUseEver);
-                ImGui.Begin("Pixel Perfect Config", ref config);
-                ImGui.Checkbox("Hitbox", ref enabled);
-                ImGui.Checkbox("Outer Ring", ref circle);
-                ImGui.Checkbox("Combat Only", ref combat);
-                ImGui.Checkbox("Instance Only", ref instance);
-                ImGui.ColorEdit4("Colour", ref col, ImGuiColorEditFlags.NoInputs);
-                ImGui.ColorEdit4("Outer Colour", ref col2, ImGuiColorEditFlags.NoInputs);
-
+                ImGui.Begin("Pixel Perfect Config", ref _config);
+                ImGui.Checkbox("Hitbox", ref _enabled);
+                ImGui.Checkbox("Outer Ring", ref _circle);
+                ImGui.Checkbox("Combat Only", ref _combat);
+                ImGui.Checkbox("Instance Only", ref _instance);
+                ImGui.ColorEdit4("Colour", ref _col, ImGuiColorEditFlags.NoInputs);
+                ImGui.ColorEdit4("Outer Colour", ref _col2, ImGuiColorEditFlags.NoInputs);
                 ImGui.Separator();
-                
-                ImGui.Checkbox("Ring", ref ring);
-                ImGui.DragFloat("Yalms", ref radius);
-                ImGui.DragFloat("Thickness", ref thickness);
-                ImGui.DragInt("Smoothness", ref segments);
-                ImGui.ColorEdit4("Ring Colour", ref col_ring, ImGuiColorEditFlags.NoInputs);
+                ImGui.Checkbox("Ring", ref _ring);
+                ImGui.DragFloat("Yalms", ref _radius);
+                ImGui.DragFloat("Thickness", ref _thickness);
+                ImGui.DragInt("Smoothness", ref _segments);
+                ImGui.ColorEdit4("Ring Colour", ref _colRing, ImGuiColorEditFlags.NoInputs);
 
                 if (ImGui.Button("Save and Close Config"))
                 {
                     SaveConfig();
-                    config = false;
+                    _config = false;
                 }
+                ImGui.SameLine();
+                ImGui.PushStyleColor(ImGuiCol.Button, 0xFF000000 | 0x005E5BFF);
+                ImGui.PushStyleColor(ImGuiCol.ButtonActive, 0xDD000000 | 0x005E5BFF);
+                ImGui.PushStyleColor(ImGuiCol.ButtonHovered, 0xAA000000 | 0x005E5BFF);
+
+                if (ImGui.Button("Buy Haplo a Hot Chocolate"))
+                {
+                    System.Diagnostics.Process.Start("https://ko-fi.com/haplo");
+                }
+                ImGui.PopStyleColor(3);
                 ImGui.End();
             }
 
-
-            if (enabled && pluginInterface.ClientState.LocalPlayer != null)
+            if (!_enabled || _pluginInterface.ClientState.LocalPlayer == null) return;
+            if(_combat)
             {
-                if(combat)
-                {
-                    if (!pluginInterface.ClientState.Condition[Dalamud.Game.ClientState.ConditionFlag.InCombat]) { return; }
-                }
-
-                if (instance)
-                {
-                    if (!pluginInterface.ClientState.Condition[Dalamud.Game.ClientState.ConditionFlag.BoundByDuty]) { return; }
-                }
-
-                var actor = pluginInterface.ClientState.LocalPlayer;
-                if (pluginInterface.Framework.Gui.WorldToScreen(new SharpDX.Vector3(actor.Position.X, actor.Position.Z, actor.Position.Y), out SharpDX.Vector2 pos))
-                {
-                    ImGui.Begin("Pixel Perfect", ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoInputs);
-                    ImGui.SetWindowPos(new Num.Vector2(pos.X - 10, pos.Y - 10));
-                    ImGui.GetWindowDrawList().AddCircleFilled(
-                        new Num.Vector2(pos.X, pos.Y),
-                        2f,
-                        ImGui.GetColorU32(col),
-                        100);
-                    if(circle)
-                    {
-                        ImGui.GetWindowDrawList().AddCircle(
-                            new Num.Vector2(pos.X, pos.Y),
-                            2.2f,
-                            ImGui.GetColorU32(col2),
-                            100);
-                    }
-
-                    ImGui.End();
-
-                    if (ring)
-                    {
-                        ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Num.Vector2(0, 0));
-                        ImGui.Begin("Ring", ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoNav | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoBackground);
-                        ImGui.SetWindowPos(new Num.Vector2(0, 0));
-                        ImGui.SetWindowSize(ImGui.GetIO().DisplaySize);
-                        DrawRingWorld(pluginInterface.ClientState.LocalPlayer, radius, segments, thickness, ImGui.GetColorU32(col_ring));
-                        ImGui.End();
-                        ImGui.PopStyleVar();
-                    }
-                }
+                if (!_pluginInterface.ClientState.Condition[Dalamud.Game.ClientState.ConditionFlag.InCombat]) { return; }
             }
-        }
-        private void ConfigWindow(object Sender, EventArgs args)
-        {
-            config = true;
-        }
 
-        public void Command(string command, string arguments)
-        {
-            config = true;
-        }
-        public void SaveConfig()
-        {
-            Configuration.Enabled = enabled;
-            Configuration.Combat = combat;
-            Configuration.Circle = circle;
-            Configuration.Instance = instance;
-            Configuration.Col = col;
-            Configuration.Col2 = col2;
-            Configuration.Col_Ring = col_ring;
-            Configuration.Thickness = thickness;
-            Configuration.Segments = segments;
-            Configuration.Ring = ring;
-            Configuration.Radius = radius;
-            this.pluginInterface.SavePluginConfig(Configuration);
-        }
-
-        public void DrawRingWorld(Dalamud.Game.ClientState.Actors.Types.Actor actor, float radius, int num_segments, float thicc, uint colour)
-        {
-            int seg = num_segments / 2;
-
-            for (int i = 0; i <= num_segments; i++)
+            if (_instance)
             {
-                pluginInterface.Framework.Gui.WorldToScreen(new SharpDX.Vector3(actor.Position.X + (radius * (float)Math.Sin((Math.PI / seg) * i)), actor.Position.Z, actor.Position.Y + (radius * (float)Math.Cos((Math.PI / seg) * i))), out SharpDX.Vector2 pos);
+                if (!_pluginInterface.ClientState.Condition[Dalamud.Game.ClientState.ConditionFlag.BoundByDuty]) { return; }
+            }
+
+            var actor = _pluginInterface.ClientState.LocalPlayer;
+            if (!_pluginInterface.Framework.Gui.WorldToScreen(
+                new SharpDX.Vector3(actor.Position.X, actor.Position.Z, actor.Position.Y),
+                out var pos)) return;
+            ImGuiHelpers.SetNextWindowPosRelativeMainViewport(new Num.Vector2(pos.X - 10 - ImGuiHelpers.MainViewport.Pos.X, pos.Y - 10- ImGuiHelpers.MainViewport.Pos.Y));
+            ImGui.Begin("Pixel Perfect", ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoBackground);
+            ImGui.GetWindowDrawList().AddCircleFilled(
+                new Num.Vector2(pos.X, pos.Y),
+                2f,
+                ImGui.GetColorU32(_col),
+                100);
+            if(_circle)
+            {
+                ImGui.GetWindowDrawList().AddCircle(
+                    new Num.Vector2(pos.X, pos.Y),
+                    2.2f,
+                    ImGui.GetColorU32(_col2),
+                    100);
+            }
+            ImGui.End();
+
+            if (!_ring) return;
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Num.Vector2(0, 0));
+            ImGuiHelpers.SetNextWindowPosRelativeMainViewport(new Num.Vector2(0, 0));
+            ImGui.Begin("Ring", ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoNav | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoBackground);
+            ImGui.SetWindowSize(ImGui.GetIO().DisplaySize);
+            DrawRingWorld(_pluginInterface.ClientState.LocalPlayer, _radius, _segments, _thickness, ImGui.GetColorU32(_colRing));
+            ImGui.End();
+            ImGui.PopStyleVar();
+        }
+        private void ConfigWindow(object sender, EventArgs args)
+        {
+            _config = true;
+        }
+
+        private void Command(string command, string arguments)
+        {
+            _config = true;
+        }
+
+        private void SaveConfig()
+        {
+            _configuration.Enabled = _enabled;
+            _configuration.Combat = _combat;
+            _configuration.Circle = _circle;
+            _configuration.Instance = _instance;
+            _configuration.Col = _col;
+            _configuration.Col2 = _col2;
+            _configuration.ColRing = _colRing;
+            _configuration.Thickness = _thickness;
+            _configuration.Segments = _segments;
+            _configuration.Ring = _ring;
+            _configuration.Radius = _radius;
+            _pluginInterface.SavePluginConfig(_configuration);
+        }
+
+        private void DrawRingWorld(Dalamud.Game.ClientState.Actors.Types.Actor actor, float radius, int numSegments, float thicc, uint colour)
+        {
+            var seg = numSegments / 2;
+            for (var i = 0; i <= numSegments; i++)
+            {
+                _pluginInterface.Framework.Gui.WorldToScreen(new SharpDX.Vector3(actor.Position.X + (radius * (float)Math.Sin((Math.PI / seg) * i)), actor.Position.Z, actor.Position.Y + (radius * (float)Math.Cos((Math.PI / seg) * i))), out SharpDX.Vector2 pos);
                 ImGui.GetWindowDrawList().PathLineTo(new Num.Vector2(pos.X, pos.Y));
             }
-            ImGui.GetWindowDrawList().PathStroke(colour, true, thicc);
-
+            ImGui.GetWindowDrawList().PathStroke(colour, ImDrawFlags.Closed, thicc);
         }
-
-
     }
 
     public class Config : IPluginConfiguration
@@ -186,14 +178,14 @@ namespace PixelPerfect
         public int Version { get; set; } = 0;
         public bool Enabled { get; set; } = true;
         public bool Combat { get; set; } = true;
-        public bool Circle { get; set; } = false;
-        public bool Instance { get; set; } = false;
+        public bool Circle { get; set; }
+        public bool Instance { get; set; }
         public Num.Vector4 Col { get; set; } = new Num.Vector4(1f, 1f, 1f, 1f);
         public Num.Vector4 Col2 { get; set; } = new Num.Vector4(0.4f, 0.4f, 0.4f, 1f);
-        public Num.Vector4 Col_Ring { get; set; } = new Num.Vector4(0.4f, 0.4f, 0.4f, 0.5f);
+        public Num.Vector4 ColRing { get; set; } = new Num.Vector4(0.4f, 0.4f, 0.4f, 0.5f);
         public int Segments { get; set; } = 100;
         public float Thickness { get; set; } = 10f;
-        public bool Ring { get; set; } = false;
+        public bool Ring { get; set; }
         public float Radius { get; set; } = 2f;
 
 
